3eae31222559d18a2348e58d66ba383b1abd1773
Karthik Gunasekaran <karthikg@cloudassert.com>
2023-05-27 11:14:06 +0530

    Initial commit: Updated basics for Mr.Cool app

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,11 +15,11 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const prompt = req.body.prompt || '';
+  if (prompt.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid message",
       }
     });
     return;
@@ -27,12 +27,12 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      model: "text-ada-001",
+      prompt: prompt,
+      temperature: 0.5,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -46,17 +46,4 @@ export default async function (req, res) {
       });
     }
   }
-}
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [promptInput, setPromptInput] = useState("");
+  const [questionList, setQuestionList] = useState([]);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -14,7 +17,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ prompt: promptInput }),
       });
 
       const data = await response.json();
@@ -22,36 +25,75 @@ export default function Home() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      const newQuestion = {
+        id: uuidv4(),
+        prompt: promptInput,
+        errorMessage: "",
+        response: data.result,
+      };
+
+      setQuestionList([...questionList, newQuestion]);
+      setPromptInput("");
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
   }
 
+  // Function to remove a question from the list
+  const removeQuestion = (id) => {
+    const updatedList = questionList.filter((question) => question.id !== id);
+    setQuestionList(updatedList);
+  };
+
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Mr.Cool - OpenAI Fine-Tuning</title>
+        <link rel="icon" href="/dhoni.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/dhoni.png" className={styles.icon} />
+        <h3>Mr.Cool</h3>
+        <ul className={styles.result} style={{ listStyleType: "none" }}>
+          {questionList.map((question, index) => (
+            <li key={question.id}>
+              <div className={styles.listItem}>
+                <div className={styles.question}>
+                  <div className={styles.row}>
+                    <strong>Prompt:</strong> {question.prompt}
+                  </div>
+                  <div className={styles.row}>
+                    <strong>Response:</strong> {question.response}
+                  </div>
+                  {question.errorMessage && (
+                    <div className={styles.row}>
+                      <strong>Error Message:</strong> {question.errorMessage}
+                    </div>
+                  )}
+                </div>
+                <FontAwesomeIcon
+                  icon={faTrash}
+                  className={styles.removeIcon}
+                  onClick={() => removeQuestion(question.id)}
+                />
+              </div>
+              {index !== questionList.length - 1 && <hr className={styles.separator} />}
+            </li>
+          ))}
+        </ul>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="prompt"
+            placeholder="Ask me anything!"
+            value={promptInput}
+            onChange={(e) => setPromptInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Submit" />
         </form>
-        <div className={styles.result}>{result}</div>
       </main>
     </div>
   );
